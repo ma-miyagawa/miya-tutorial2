9ee9b7f414386279b03a924ad755da91e26542c6
ma-miyagawa <miyagawa@esm.co.jp>
2022-11-05 10:33:09 +0900

    ＷＥＢアプリ開発２（１－１）書籍整理の一覧画面の作成

--- web_application_2/src/view/BookApp.vue
+++ web_application_2/src/view/BookApp.vue
@@ -1,16 +1,72 @@
 <template>
   <div>
-    書籍管理アプリだよ
+    <v-data-table
+      :headers="headers"
+      :items="desserts"
+      :items-per-page="5"
+      class="elevation-1"
+    >
+      <template v-slot:[`item.update`]="{ item }">
+        <v-btn
+          tile
+          color="secondary"
+          v-on:click="editItem(item)"
+        >
+          <v-icon left>
+            mdi-pencil
+          </v-icon>
+          更新
+        </v-btn>
+      </template>
+      <template v-slot:[`item.delete`]="{ item }">
+        <v-btn
+          tile
+          color="success"
+          v-on:click="deleteItem(item)"
+        >
+          <v-icon left>
+            mdi-delete
+          </v-icon>
+          削除
+        </v-btn>
+      </template>
+    </v-data-table>
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {}
+    return {
+      headers: [
+        { text: 'タイトル', align: 'start', sortable: false, value: 'title', width: '30%' },
+        { text: 'ジャンル', align: 'start', sortable: false, value: 'genre', width: '20%' },
+        { text: '購入日', align: 'start', sortable: false, value: 'purchaseDate', width: '15%' },
+        { text: '購入者', align: 'start', sortable: false, value: 'buyer', width: '15%' },
+        { text: '更新', sortable: false, value: 'update', width: '5%' },
+        { text: '削除', sortable: false, value: 'delete', width: '5%' }
+      ]
+    }
   },
-  created () {},
-  computed: {},
-  methods: {}
+  created () {
+    this.initialize()
+  },
+  methods: {
+    initialize () {
+      this.desserts = [
+        { title: 'タイトル１', genre: 'ジャンル１', purchaseDate: '2022/11/11', buyer: '宮川' },
+        { title: 'タイトル２', genre: 'ジャンル１', purchaseDate: '2022/11/12', buyer: '松尾' },
+        { title: 'タイトル３', genre: 'ジャンル２', purchaseDate: '2022/11/13', buyer: '嶋田' },
+        { title: 'タイトル４', genre: 'ジャンル１', purchaseDate: '2022/11/14', buyer: '横山' },
+        { title: 'タイトル４', genre: 'ジャンル１', purchaseDate: '2022/11/15', buyer: '轟' },
+        { title: 'タイトル４', genre: 'ジャンル１', purchaseDate: '2022/11/16', buyer: '野瀬' },
+        { title: 'タイトル２', genre: 'ジャンル２', purchaseDate: '2022/11/17', buyer: '西埜' }
+      ]
+    },
+    editItem (item) {
+    },
+    deleteItem  (item) {
+    }
+  }
 }
 </script>
